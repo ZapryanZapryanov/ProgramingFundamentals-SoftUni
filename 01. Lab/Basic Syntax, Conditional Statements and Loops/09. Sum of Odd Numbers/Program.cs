f2215be7f4e96da0fd1cc1c756907b07a44b5a6a
zaprian1223 <zaprian1223@gmail.com>
2022-04-25 14:45:33 +0300

    Added a solution

---
+++ 01. Lab/Basic Syntax, Conditional Statements and Loops/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int all = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum = 1 + (i * 2);
+                all += sum;
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine($"Sum: {all}");
+        }
+    }
+}
