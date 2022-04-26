7e037377b97963aaa878b37bcdcd3dd063f5344f
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:03:19 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            double x = 0;
+            double result = 0;
+
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            x = double.Parse(Console.ReadLine());
+            result = (a * b * x) / 3;
+            Console.Write($"Length: Width: Height: Pyramid Volume: {result:f2}");
+        }
+    }
+}
