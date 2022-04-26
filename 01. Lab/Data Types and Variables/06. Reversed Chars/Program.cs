56b6f8e90baee5edaadbe2fdebc79f6aadc32791
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:01:58 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/06. Reversed Chars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _06._Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char simbyl1 = char.Parse(Console.ReadLine());
+            char simbyl2 = char.Parse(Console.ReadLine());
+            char simbyl3 = char.Parse(Console.ReadLine());
+            Console.Write($"{simbyl3} {simbyl2} {simbyl1}");
+        }
+    }
+}
