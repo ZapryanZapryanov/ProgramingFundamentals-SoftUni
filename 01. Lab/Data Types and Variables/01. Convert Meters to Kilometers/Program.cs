2cc2dcd92e52057066b5886dbdc7ca6d098d3161
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:00:40 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            decimal kilometars = (decimal)(number / 1000);
+            Console.WriteLine($"{kilometars:f2}");
+        }
+    }
+}
