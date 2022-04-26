f57eec604935ff87b7f12f02bd4eab5abe2cd16f
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:02:14 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/07. Concat Names/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string name3 = Console.ReadLine();
+            Console.Write($"{name1}{name3}{name2}");
+        }
+    }
+}
