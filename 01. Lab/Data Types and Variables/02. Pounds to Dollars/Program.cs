76f0199c017a4ac78f075ddc569f4b725541a5cc
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:00:54 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+
+            decimal translete = (decimal)(money * 1.31);
+            Console.WriteLine($"{translete:f3}");
+        }
+    }
+}
