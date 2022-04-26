e725da9ce54e716e6ea5fc23e6a6895098623ce4
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:01:27 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Minutes = 60;
+            const int Hours = 24;
+            const decimal Days = 365.2422m;
+            short sum = short.Parse(Console.ReadLine());
+            short centuries = (short)(sum * 100);
+            int years = (int)(centuries * Days);
+            long days = (long)(years * Hours);
+            BigInteger hours = (BigInteger)(days * Minutes);
+
+            Console.Write($"{sum} centuries = {centuries} years = {years} days = {days} hours = {hours} minutes");
+
+        }
+    }
+}
