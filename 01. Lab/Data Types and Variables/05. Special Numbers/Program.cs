bb26c06fdb3824f3143a5200b404983a34cef16b
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:01:44 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/05. Special Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = i;
+                int curent = 0;
+                while (sum != 0)
+                {
+                    curent += sum % 10;
+                    sum /= 10;
+                }
+                if (curent == 5 || curent == 7 || curent == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+    }
+}
