46497a44575238db1e69d07c7f23477ecc212ae0
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:03:00 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/10. Lower or Upper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbyl = char.Parse(Console.ReadLine());
+            if (symbyl >= 65 && symbyl <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
