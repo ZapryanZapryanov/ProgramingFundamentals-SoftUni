eef4264ae9d828e000c5125cc5032f18ff18c45c
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:02:45 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/09. Chars to String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbyl = char.Parse(Console.ReadLine());
+            char symby2 = char.Parse(Console.ReadLine());
+            char symby3 = char.Parse(Console.ReadLine());
+            Console.Write($"{symbyl}{symby2}{symby3}");
+        }
+    }
+}
