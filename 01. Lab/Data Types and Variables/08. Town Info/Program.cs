260cc07d5058ffd6e5226d3d32b4a4c7aca76848
zaprian1223 <zaprian1223@gmail.com>
2022-04-26 18:02:30 +0300

    Added a solution

---
+++ 01. Lab/Data Types and Variables/08. Town Info/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08._Town_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            long people = long.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {town} has population of {people} and area {area} square km.");
+        }
+    }
+}
