0ac9f208c5f2ff5b35e36797e7f2d3a6e1c94638
zaprian1223 <zaprian1223@gmail.com>
2022-04-27 17:42:35 +0300

    Added a solution

---
+++ 01. Lab/Arrays/01. Day of Week/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string[] daysWeekArrays = new string[7]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            if (num > 0 && num <= daysWeekArrays.Length)
+            {
+                Console.WriteLine(daysWeekArrays[num - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
