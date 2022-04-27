476d8f72e6003b13446a9b5e6aa972b66a72269f
zaprian1223 <zaprian1223@gmail.com>
2022-04-27 17:44:36 +0300

    Added a solution

---
+++ 01. Lab/Arrays/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < items.Length; i++)
+            {
+                int currItems = items[i];
+                if (currItems % 2 == 0)
+                {
+                    evenSum += currItems;
+                }
+                else
+                {
+                    oddSum += currItems;
+                }
+            }
+            int allSum = evenSum - oddSum;
+            Console.WriteLine(allSum);
+        }
+    }
+}
