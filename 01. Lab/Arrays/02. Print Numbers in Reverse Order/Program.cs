cef58fa1c6a6e049941fcab85f63ed1eceb6ed2d
zaprian1223 <zaprian1223@gmail.com>
2022-04-27 17:43:03 +0300

    Added a solution

---
+++ 01. Lab/Arrays/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arraysa = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arraysa[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = arraysa.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{arraysa[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
