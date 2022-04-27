6bea72ddc8e9ad565d8a4e5c92475c7fc079fe35
zaprian1223 <zaprian1223@gmail.com>
2022-04-27 17:43:26 +0300

    Added a solution

---
+++ 01. Lab/Arrays/03. Rounding Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            double[] arrayItems = new double[rawInput.Length];
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                arrayItems[i] = double.Parse(rawInput[i]);
+
+            }
+            for (int i = 0; i < arrayItems.Length; i++)
+            {
+                Console.WriteLine($"{arrayItems[i]} => {Math.Round(arrayItems[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
