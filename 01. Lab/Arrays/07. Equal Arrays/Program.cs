e166e08b7a4de7b064b81c1cff6a3b4c0865f9b6
zaprian1223 <zaprian1223@gmail.com>
2022-04-27 17:44:59 +0300

    Added a solution

---
+++ 01. Lab/Arrays/07. Equal Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07._Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraysOne = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arraysSecond = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i < arraysOne.Length; i++)
+            {
+                sum += arraysOne[i];
+                if (arraysOne[i] != arraysSecond[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
