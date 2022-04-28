f01ff98307faf88c680b69ef4d233be4e4969023
zaprian1223 <zaprian1223@gmail.com>
2022-04-28 18:21:42 +0300

    Added a solution

---
+++ 01. Lab/Methods/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+            Console.WriteLine(getCalculate(first, second));
+        }
+        static double getCalculate(double width, double hight)
+        {
+            return width * hight;
+        }
+    
+    }
+}
