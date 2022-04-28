919fb7bf74121b2523109bfd58e5ad64bf395507
zaprian1223 <zaprian1223@gmail.com>
2022-04-28 18:22:20 +0300

    Added a solution

---
+++ 01. Lab/Methods/08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bases = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMathPow(bases, power));
+        }
+        static int GetMathPow(int basis, int power)
+        {
+            int result = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                result *= basis;
+            }
+            return result;
+        }
+    }
+}
