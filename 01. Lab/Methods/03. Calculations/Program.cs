c733d7d0b4a750e80cbdcdd877a83f998243cc9a
zaprian1223 <zaprian1223@gmail.com>
2022-04-28 18:20:48 +0300

    Added a solution

---
+++ 01. Lab/Methods/03. Calculations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            switch (comand)
+            {
+                case "add":
+                    printAdd(first, second);
+                    break;
+                case "multiply":
+                    printMultiply(first, second);
+                    break;
+                case "subtract":
+                    printSubtract(first, second);
+                    break;
+                case "divide":
+                    printDivide(first, second);
+
+                    break;
+            }
+
+        }
+        static void printAdd(int first, int second)
+        {
+            Console.WriteLine(first + second);
+        }
+        static void printMultiply(int first, int second)
+        {
+            Console.WriteLine(first * second);
+
+        }
+        static void printSubtract(int first, int second)
+        {
+            Console.WriteLine(first - second);
+
+        }
+        static void printDivide(int first, int second)
+        {
+            Console.WriteLine(first / second);
+
+        }
+    
+    }
+}
