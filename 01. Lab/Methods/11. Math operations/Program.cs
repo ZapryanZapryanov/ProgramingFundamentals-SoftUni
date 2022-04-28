2f9bb8f8f22de547de68a82c1954333fa2c12153
zaprian1223 <zaprian1223@gmail.com>
2022-04-28 18:23:22 +0300

    Added a solution

---
+++ 01. Lab/Methods/11. Math operations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateOne(firstNum, symbol, secondNum));
+        }
+        static double CalculateOne(int first, char sym, int second)
+        {
+            switch (sym)
+            {
+                case '*':
+                    first *= second;
+                    break;
+                case '/':
+                    first /= second;
+                    break;
+                case '+':
+                    first += second;
+                    break;
+                case '-':
+                    first -= second;
+                    break;
+            }
+            return first;
+        }
+    }
+}
