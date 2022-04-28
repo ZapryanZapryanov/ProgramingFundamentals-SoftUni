3b5301243cbc6e0487f234779306dea3e3e62099
zaprian1223 <zaprian1223@gmail.com>
2022-04-28 18:22:01 +0300

    Added a solution

---
+++ 01. Lab/Methods/07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            int rotation = int.Parse(Console.ReadLine());
+            string finalyy = PrintStrings(comand, rotation);
+            Console.WriteLine(finalyy);
+        }
+        static string PrintStrings(string comand, int rotation)
+        {
+            StringBuilder stringRotation = new StringBuilder();
+            for (int i = 0; i < rotation; i++)
+            {
+                stringRotation.Append(comand);
+            }
+            return stringRotation.ToString();
+        }
+    }
+}
