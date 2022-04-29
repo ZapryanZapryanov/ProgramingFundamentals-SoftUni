7ba6b460f6176a6e42bfa497e0b794ff2e62ce20
zaprian1223 <zaprian1223@gmail.com>
2022-04-29 18:53:59 +0300

    Added a solution

---
+++ 01. Lab/Lists/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._List_Manipulation_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string comand = Console.ReadLine();
+            while (comand != "end")
+            {
+                string[] program = comand.Split(" ");
+                string firstElements = program[0];
+                if (firstElements == "Add")
+                {
+                    int elements = int.Parse(program[1]);
+                    list.Add(elements);
+                }
+                else if (firstElements == "Remove")
+                {
+                    int number = int.Parse(program[1]);
+                    list.Remove(number);
+                }
+                else if (firstElements == "RemoveAt")
+                {
+                    int num = int.Parse(program[1]);
+                    list.RemoveAt(num);
+                }
+                else if (firstElements == "Insert")
+                {
+                    int num = int.Parse(program[1]);
+                    int number = int.Parse(program[2]);
+                    list.Insert(number, num);
+                }
+
+
+
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
