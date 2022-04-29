f0421dcfbaedf7f134da51ca100bdc283c5a8572
zaprian1223 <zaprian1223@gmail.com>
2022-04-29 18:52:51 +0300

    Added a solution

---
+++ 01. Lab/Lists/02. Gauss' Trick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arrList = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < arrList.Count / 2; i++)
+            {
+                int sum = arrList[i] + arrList[arrList.Count - i - 1];
+                numbers.Add(sum);
+            }
+            if (arrList.Count % 2 != 0)
+            {
+                numbers.Add(arrList[arrList.Count / 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
