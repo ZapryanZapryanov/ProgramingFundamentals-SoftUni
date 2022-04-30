bcd9db9cb91511e70c77d81c56241a6b1ed3aa7c
zaprian1223 <zaprian1223@gmail.com>
2022-04-30 17:04:44 +0300

    Added a solution

---
+++ 01. Lab/Objects and Classes/04. Students/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Students
+{
+    class Student
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            List<Student> listStudents = new List<Student>();
+            while (comand != "end")
+            {
+                string[] element = comand.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string firstName = element[0];
+                string lastName = element[1];
+                int age = int.Parse(element[2]);
+                string homeTown = element[3];
+                Student oneStudent = new Student();
+                oneStudent.FirstName = firstName;
+                oneStudent.LastName = lastName;
+                oneStudent.Age = age;
+                oneStudent.HomeTown = homeTown;
+                listStudents.Add(oneStudent);
+
+
+
+                comand = Console.ReadLine();
+            }
+            string town = Console.ReadLine();
+            List<Student> newList = listStudents.FindAll(x => x.HomeTown == town);
+            foreach (var item in newList)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} is {item.Age} years old.");
+            }
+        }
+    }
+}
