b14dd0686d6c82c7ea26857b917c052e7c9d6469
zaprian1223 <zaprian1223@gmail.com>
2022-04-30 17:04:08 +0300

    Added a solution

---
+++ 01. Lab/Objects and Classes/02. Big Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger numbers = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                numbers *= i;
+            }
+            Console.WriteLine(numbers);
+        }
+    }
+}
