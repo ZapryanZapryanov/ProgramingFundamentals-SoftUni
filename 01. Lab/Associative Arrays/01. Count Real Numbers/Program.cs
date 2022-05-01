aa9cb93593ae782be54ec734628ca392cd018717
zaprian1223 <zaprian1223@gmail.com>
2022-05-01 14:06:44 +0300

    Added a solution

---
+++ 01. Lab/Associative Arrays/01. Count Real Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(x => int.Parse(x)).ToList();
+            SortedDictionary<int, int> arr = new SortedDictionary<int, int>();
+            foreach (var item in list)
+            {
+                if (arr.ContainsKey(item))
+                {
+                    arr[item] += 1;
+                }
+                else
+                {
+                    arr.Add(item, 1);
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+      
+         }
+    }
+
