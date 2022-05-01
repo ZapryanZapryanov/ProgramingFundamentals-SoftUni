7e049986ce1e604afbd82903b3c280c281a2ffdb
zaprian1223 <zaprian1223@gmail.com>
2022-05-01 14:07:04 +0300

    Added a solution

---
+++ 01. Lab/Associative Arrays/02. Odd Occurrences/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split().ToList();
+            Dictionary<string, int> arr = new Dictionary<string, int>();
+            foreach (var item in list)
+            {
+                string lowerCase = item.ToLower();
+                if (arr.ContainsKey(lowerCase))
+                {
+                    arr[lowerCase] += 1;
+                }
+                else
+                {
+                    arr.Add(lowerCase, 1);
+                }
+            }
+            List<string> result = arr.Where(item => item.Value % 2 != 0).Select(item => item.Key).ToList();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
