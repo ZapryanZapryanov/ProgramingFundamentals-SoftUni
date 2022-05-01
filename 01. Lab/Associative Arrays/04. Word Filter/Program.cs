f6f96072175779a011c286386c0b696cc313cf1e
zaprian1223 <zaprian1223@gmail.com>
2022-05-01 14:07:44 +0300

    Added a solution

---
+++ 01. Lab/Associative Arrays/04. Word Filter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(x => x.Length % 2 == 0).ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
