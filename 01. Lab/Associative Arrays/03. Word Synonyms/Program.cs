04062c7775e4585af7d9a1abb17acc872c902818
zaprian1223 <zaprian1223@gmail.com>
2022-05-01 14:07:23 +0300

    Added a solution

---
+++ 01. Lab/Associative Arrays/03. Word Synonyms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string sinonim = Console.ReadLine();
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word].Add(sinonim);
+                }
+                else
+                {
+                    List<string> newList = new List<string>() { sinonim };
+                    dictionary.Add(word, newList);
+                }
+
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
