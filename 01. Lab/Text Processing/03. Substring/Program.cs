46943e7e63509acceddcbf2c8ad3aa1f2d0f9c5a
zaprian1223 <zaprian1223@gmail.com>
2022-05-02 13:54:27 +0300

    Added a solution

---
+++ 01. Lab/Text Processing/03. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            while (second.Contains(first))
+            {
+                int index = 0;
+                index = second.IndexOf(first);
+                second = second.Remove(index, first.Length);
+            }
+            Console.WriteLine(second);
+        }
+    }
+}
