6f76da1519ab944e9f07b36401cddcbb1bebcc59
zaprian1223 <zaprian1223@gmail.com>
2022-05-02 13:54:10 +0300

    Added a solution

---
+++ 01. Lab/Text Processing/02. Repeat Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder text = new StringBuilder();
+
+
+            foreach (var item in arr)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    text.Append(item);
+
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
