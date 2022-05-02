0bd1dc9e91f83c6daa4eb203bd79035b68e37a3c
zaprian1223 <zaprian1223@gmail.com>
2022-05-02 13:53:53 +0300

    Added a solution

---
+++ 01. Lab/Text Processing/01. Reverse Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+
+            while (true)
+            {
+
+                if (comand == "end")
+                {
+                    break;
+                }
+                string words = string.Empty;
+                for (int i = comand.Length - 1; i >= 0; i--)
+                {
+                    words += comand[i];
+                }
+                Console.WriteLine($"{comand} = {words}");
+                comand = Console.ReadLine();
+
+            }
+        }
+         
+        
+        
+    }
+}
