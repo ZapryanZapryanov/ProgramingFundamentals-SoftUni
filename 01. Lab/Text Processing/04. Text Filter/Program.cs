29138c79f56a8b0606274ce4528e4843d32a31b9
zaprian1223 <zaprian1223@gmail.com>
2022-05-02 13:54:43 +0300

    Added a solution

---
+++ 01. Lab/Text Processing/04. Text Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> banedWords = Console.ReadLine().Split(", ").ToList();
+            string text = Console.ReadLine();
+            foreach (var item in banedWords)
+            {
+                text = text.Replace(item, new string('*', item.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
