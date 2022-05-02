4d1dd70f41e63cf4fa32b6e29de4055347dd6938
zaprian1223 <zaprian1223@gmail.com>
2022-05-02 13:54:59 +0300

    Added a solution

---
+++ 01. Lab/Text Processing/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder numbers = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder others = new StringBuilder();
+
+            foreach (Char ch in text)
+            {
+                if (char.IsDigit(ch))
+                {
+                    numbers.Append(ch);
+                }
+                else if (char.IsLetter(ch))
+                {
+                    letters.Append(ch);
+                }
+                else
+                {
+                    others.Append(ch);
+                }
+            }
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
