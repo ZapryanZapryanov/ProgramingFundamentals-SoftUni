aacab29a83ff7c9e7ba1ce169b4b5bddfd402f07
zaprian1223 <zaprian1223@gmail.com>
2022-05-03 18:07:39 +0300

    Added a solution

---
+++ 01. Lab/Regular Expressions/02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+
+            MatchCollection words = Regex.Matches(text, @"\+359( |-)2\1\d{3}\1\d{4}\b");
+
+            string[] arr = words.Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
