c9ffdc4e69c9d4fd2d5789805c61dd8dd3bb5127
zaprian1223 <zaprian1223@gmail.com>
2022-05-03 18:07:24 +0300

    Added a solution

---
+++ 01. Lab/Regular Expressions/01. Match Full Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+
+            MatchCollection word = Regex.Matches(text, @"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+
+            foreach (Match item in word)
+            {
+                Console.Write($"{item.Value} ");
+            }
+        }
+    }
+}
