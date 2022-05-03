08d34ca69d1f2a296100236e4c9f749004838db2
zaprian1223 <zaprian1223@gmail.com>
2022-05-03 18:07:56 +0300

    Added a solution

---
+++ 01. Lab/Regular Expressions/03. Match Dates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            MatchCollection colect = Regex.Matches(text, @"(?<day>\d{2})(.|-|/)(?<month>[A-Z][a-z]{2})\1(?<years>\d{4})");
+
+            foreach (Match item in colect)
+            {
+                Console.WriteLine($"Day: {item.Groups["day"].Value}, Month: {item.Groups["month"].Value}, Year: {item.Groups["years"].Value}");
+            }
+    }
+}
