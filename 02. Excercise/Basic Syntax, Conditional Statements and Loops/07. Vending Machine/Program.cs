2439252b87dbb77b4f5d09df35b16507a6fb176a
zaprian1223 <zaprian1223@gmail.com>
2022-05-04 17:28:09 +0300

    Added a solution

---
+++ 02. Excercise/Basic Syntax, Conditional Statements and Loops/07. Vending Machine/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace _07._Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string money = Console.ReadLine();
+            double sum = 0;
+
+            while (money != "Start")
+            {
+                double price = double.Parse(money);
+                switch (price)
+                {
+                    case 0.1:
+                        sum += 0.10;
+                        break;
+                    case 0.2:
+                        sum += 0.20;
+                        break;
+                    case 0.5:
+                        sum += 0.50;
+                        break;
+                    case 1:
+                        sum += 1;
+                        break;
+                    case 2:
+                        sum += 2;
+                        break;
+                    default:
+                        Console.WriteLine($"Cannot accept {price}");
+                        break;
+                }
+
+                money = Console.ReadLine();
+            }
+            string product = Console.ReadLine();
+            while (product != "End")
+            {
+                switch (product)
+                {
+                    case "Nuts":
+                        if (sum >= 2)
+                        {
+                            sum -= 2.0;
+                            Console.WriteLine($"Purchased nuts");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+
+                        break;
+                    case "Water":
+                        if (sum >= 0.70)
+                        {
+                            sum -= 0.70;
+                            Console.WriteLine($"Purchased water");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+
+                        break;
+                    case "Crisps":
+                        if (sum >= 1.50)
+                        {
+                            sum -= 1.50;
+                            Console.WriteLine($"Purchased crisps");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+
+                        break;
+                    case "Soda":
+                        if (sum >= 0.80)
+                        {
+                            sum -= 0.80;
+                            Console.WriteLine($"Purchased soda");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    case "Coke":
+                        if (sum >= 1)
+                        {
+                            sum -= 1.0;
+                            Console.WriteLine($"Purchased coke");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        break;
+
+                }
+                product = Console.ReadLine();
+            }
+            Console.WriteLine($"Change: {sum:f2}");
+        }
+    }
+}
