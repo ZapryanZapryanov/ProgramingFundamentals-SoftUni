8677da254f2c140ec79f09187312a81e06d4504e
zaprian1223 <zaprian1223@gmail.com>
2022-05-04 17:26:35 +0300

    Added a solution

---
+++ 02. Excercise/Basic Syntax, Conditional Statements and Loops/02. Division/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02._Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = int.Parse(Console.ReadLine());
+
+            double sum = -1;
+            if (num % 10 == 0)
+            {
+                sum = 10;
+            }
+            else if (num % 7 == 0)
+            {
+                sum = 7;
+            }
+            else if (num % 6 == 0)
+            {
+                sum = 6;
+            }
+            else if (num % 3 == 0)
+            {
+                sum = 3;
+            }
+            else if (num % 2 == 0)
+            {
+                sum = 2;
+            }
+            if (sum == -1)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {sum}");
+            }
+        }
+    }
+}
