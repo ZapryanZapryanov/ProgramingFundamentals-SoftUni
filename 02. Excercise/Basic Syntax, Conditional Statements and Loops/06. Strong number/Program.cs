8882c8aaf77ccf4c45e0f9e4cf971851719fc629
zaprian1223 <zaprian1223@gmail.com>
2022-05-04 17:27:50 +0300

    Added a solution

---
+++ 02. Excercise/Basic Syntax, Conditional Statements and Loops/06. Strong number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int all = 0;
+            bool flag = false;
+            double ah = num;
+            while (!flag)
+            {
+                sum = num % 10;
+                num /= 10;
+                int a = 1;
+                for (int i = 1; i <= sum; i++)
+                {
+                    a *= i;
+                }
+                all += a;
+                if (all == ah)
+                {
+                    Console.WriteLine("yes");
+                    return;
+                }
+                if (num == 0)
+                {
+                    flag = true;
+                }
+
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
