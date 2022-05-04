8ea52cb6fca9cc4ae59df7bf42d1630640854d96
zaprian1223 <zaprian1223@gmail.com>
2022-05-04 17:29:23 +0300

    Added a solution

---
+++ 02. Excercise/Basic Syntax, Conditional Statements and Loops/11. Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double allSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+                double PriceCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsule = int.Parse(Console.ReadLine());
+                sum += PriceCapsule * days * capsule;
+                allSum += PriceCapsule * days * capsule;
+                Console.WriteLine($"The price for the coffee is: ${sum:f2}");
+            }
+            Console.WriteLine($"Total: ${allSum:f2}");
+        }
+    }
+}
