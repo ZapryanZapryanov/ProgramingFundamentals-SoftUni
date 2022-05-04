9a57530fd3f55edb9d070bb9d4c83e5236a0138b
zaprian1223 <zaprian1223@gmail.com>
2022-05-04 17:26:12 +0300

    Added a solution

---
+++ 02. Excercise/Basic Syntax, Conditional Statements and Loops/01. Ages/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ages = int.Parse(Console.ReadLine());
+            if (0 <= ages && ages <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (ages <= 13 && ages >= 3)
+            {
+                Console.WriteLine("child");
+            }
+            else if (ages <= 19 && ages >= 14)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (ages <= 65 && ages >= 20)
+            {
+                Console.WriteLine("adult");
+            }
+            else
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
