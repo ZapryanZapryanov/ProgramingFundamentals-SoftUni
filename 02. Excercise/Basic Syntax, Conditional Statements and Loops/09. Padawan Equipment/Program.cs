fa3b7520ea66d347afecb10328ae3b7fb7e0182e
zaprian1223 <zaprian1223@gmail.com>
2022-05-04 17:28:44 +0300

    Added a solution

---
+++ 02. Excercise/Basic Syntax, Conditional Statements and Loops/09. Padawan Equipment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountOfMoney = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceLightsabel = double.Parse(Console.ReadLine());
+            double priceRobe = double.Parse(Console.ReadLine());
+            double priceBelt = double.Parse(Console.ReadLine());
+            int alls = 0;
+            double people = students;
+            for (int i = 1; i <= students; i++)
+            {
+                alls++;
+                if (alls == 6)
+                {
+                    alls = 0;
+                    people -= 1;
+
+                }
+            }
+            double all = (priceLightsabel * Math.Ceiling(students * 0.10 + students)) + (priceRobe * students) + (priceBelt * people);
+            if (all <= amountOfMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {all:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {all - amountOfMoney:f2}lv more.");
+            }
+        }
+    }
+}
