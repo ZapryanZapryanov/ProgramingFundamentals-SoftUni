277da94347ef798c7d32593fb70a0ee5e5687439
zaprian1223 <zaprian1223@gmail.com>
2022-05-04 17:27:33 +0300

    Added a solution

---
+++ 02. Excercise/Basic Syntax, Conditional Statements and Loops/05. Login/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string passrowd = string.Empty;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                passrowd += username[i];
+
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                string aa = Console.ReadLine();
+                if (passrowd == aa)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    if (i == 3)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect password. Try again.");
+                    }
+                }
+            }
+        }
+    }
+}
