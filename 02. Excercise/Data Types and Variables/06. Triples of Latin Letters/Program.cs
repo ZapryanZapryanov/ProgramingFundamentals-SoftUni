69266095c9c1da1b759c6d23014fd69bd798a68f
zaprian1223 <zaprian1223@gmail.com>
2022-05-05 17:32:40 +0300

    Added a solution

---
+++ 02. Excercise/Data Types and Variables/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int a = 0; a < n; a++)
+                {
+
+                    for (int b = 0; b < n; b++)
+                    {
+
+
+                        char first = (char)('a' + i);
+                        char first1 = (char)('a' + a);
+                        char first2 = (char)('a' + b);
+
+
+                        Console.WriteLine($"{first}{first1}{first2}");
+
+                    }
+                }
+
+            }
+        }
+    }
+}
