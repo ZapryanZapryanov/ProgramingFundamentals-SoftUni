6e920f32d909a88e24d4e5662c50e9bf5556e070
zaprian1223 <zaprian1223@gmail.com>
2022-05-05 17:31:47 +0300

    Added a solution

---
+++ 02. Excercise/Data Types and Variables/03. Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbProple = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int all = (int)Math.Ceiling((double)numbProple / capacity);
+            Console.WriteLine(all);
+        }
+    }
+}
