0a65eb7998de1e26274ee925aed7deb0570bb504
zaprian1223 <zaprian1223@gmail.com>
2022-05-05 17:35:00 +0300

    Added a solution

---
+++ 02. Excercise/Data Types and Variables/10. Poke Mon/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokemonPower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactory = int.Parse(Console.ReadLine());
+
+            int targetsPoked = 0;
+
+            double originalValue = pokemonPower * 0.50;
+
+            while (pokemonPower >= distance)
+            {
+                if (pokemonPower == originalValue)
+                {
+                    if (exhaustionFactory > 0)
+                    {
+                        pokemonPower = pokemonPower / exhaustionFactory;
+                    }
+
+                    if (pokemonPower < distance)
+                    {
+                        break;
+                    }
+                }
+
+                pokemonPower -= distance;
+                targetsPoked++;
+            }
+
+            Console.WriteLine(pokemonPower);
+            Console.WriteLine(targetsPoked);
+        }
+    }
+}
