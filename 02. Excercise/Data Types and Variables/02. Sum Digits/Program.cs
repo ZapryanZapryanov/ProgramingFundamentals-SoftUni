a84a18ee3b1b1d86603c0185e3ad72dffc58e033
zaprian1223 <zaprian1223@gmail.com>
2022-05-05 17:31:28 +0300

    Added a solution

---
+++ 02. Excercise/Data Types and Variables/02. Sum Digits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int magicNum = int.Parse(Console.ReadLine());
+            int firstNum = 0;
+            int sum = 0;
+            while (true)
+            {
+                firstNum = magicNum % 10;
+                magicNum /= 10;
+                sum += firstNum;
+                if (magicNum == 0)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+            }
+        }
+    }
+}
