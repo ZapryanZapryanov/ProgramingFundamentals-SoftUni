b543cf99c0de54aa4f576ecde3061db9ad17d260
zaprian1223 <zaprian1223@gmail.com>
2022-05-05 17:32:59 +0300

    Added a solution

---
+++ 02. Excercise/Data Types and Variables/07. Water Overflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int litters = int.Parse(Console.ReadLine());
+
+
+                if (sum <= 255)
+                {
+                    sum += litters;
+                    if (sum > 255)
+                    {
+                        sum -= litters;
+                        Console.WriteLine("Insufficient capacity!");
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
