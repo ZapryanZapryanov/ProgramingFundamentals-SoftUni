be4a18d6631e4a0f77b8fb4718ed3d8897aa7229
zaprian1223 <zaprian1223@gmail.com>
2022-05-05 17:32:21 +0300

    Added a solution

---
+++ 02. Excercise/Data Types and Variables/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05._Print_Part_Of_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int stop = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= stop; i++)
+            {
+                char symbyl = (char)i;
+                Console.Write($"{symbyl} ");
+            }
+        }
+    }
+}
