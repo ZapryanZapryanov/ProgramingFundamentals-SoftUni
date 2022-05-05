6b464ac290cb9b266498f7f6dde4ac2a60be6a87
zaprian1223 <zaprian1223@gmail.com>
2022-05-05 17:34:42 +0300

    Added a solution

---
+++ 02. Excercise/Data Types and Variables/09. Spice Must Flow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			long mine = long.Parse(Console.ReadLine());
+			long harvest = 0;
+			long days = 0;
+
+			if (mine >= 100)
+			{
+				do
+				{
+					days++;
+					harvest = (harvest + mine) - 26;
+					mine -= 10;
+				} while (mine >= 100);
+				harvest = harvest - 26;
+				Console.WriteLine(days);
+				Console.WriteLine(harvest);
+			}
+
+		}
+	}
+}
