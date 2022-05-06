692b661463635419276007d9205e74306a102168
zaprian1223 <zaprian1223@gmail.com>
2022-05-06 15:09:08 +0300

    Added a solution

---
+++ 02. Excercise/Arrays/05. Top Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            int[] big = new int[arr.Length];
+            int bigIndex = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int nextNum = arr[i];
+                bool flag = true;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int currNum = arr[j];
+                    if (currNum >= nextNum)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    big[bigIndex] = nextNum;
+                    bigIndex++;
+                }
+            }
+            for (int i = 0; i < bigIndex; i++)
+            {
+                Console.Write($"{big[i]} ");
+            }
+        }
+    }
+}
