832d06ce92bfe39dbcce2255549a9346dffda9ba
zaprian1223 <zaprian1223@gmail.com>
2022-05-06 15:08:25 +0300

    Added a solution

---
+++ 02. Excercise/Arrays/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                int[] items = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int first = items[0];
+                int second = items[1];
+                if (i % 2 == 0)
+                {
+                    firstArr[i - 1] = first;
+                    secondArr[i - 1] = second;
+                }
+                else
+                {
+                    firstArr[i - 1] = second;
+                    secondArr[i - 1] = first;
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", secondArr));
+            Console.WriteLine(string.Join(" ", firstArr));
+
+        }
+    }
+}
