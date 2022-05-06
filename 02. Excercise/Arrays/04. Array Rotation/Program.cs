bba170fd982d1a473410d1a2fc2b810c88ef17b7
zaprian1223 <zaprian1223@gmail.com>
2022-05-06 15:08:47 +0300

    Added a solution

---
+++ 02. Excercise/Arrays/04. Array Rotation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int a = 0; a < n; a++)
+            {
+                int index = arr[0];
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = index;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
