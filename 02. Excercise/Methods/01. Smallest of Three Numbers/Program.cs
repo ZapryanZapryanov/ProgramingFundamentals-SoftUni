8a5452c8f357c369e4aba7896b5d6b3ed0649c6a
zaprian1223 <zaprian1223@gmail.com>
2022-05-08 13:32:10 +0300

    Added a solution

---
+++ 02. Excercise/Methods/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMinNumber(first, second, third));
+
+        }
+        static int GetMinNumber(int firstSum, int secondSum, int thirdSum)
+        {
+            int minNum = 0;
+            if (firstSum <= secondSum && firstSum <= thirdSum)
+            {
+                minNum = firstSum;
+            }
+            else if (secondSum <= firstSum && secondSum <= thirdSum)
+            {
+                minNum = secondSum;
+            }
+            else if (thirdSum <= firstSum && thirdSum <= secondSum)
+            {
+                minNum = thirdSum;
+            }
+            return minNum;
+        }
+    }
+}
