132f007a7174698a984a8b48479b065f7aed7514
zaprian1223 <zaprian1223@gmail.com>
2022-05-08 13:32:40 +0300

    Added a solution

---
+++ 02. Excercise/Methods/03. Characters in Range/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            bool flag = true;
+            int first = (int)firstSymbol;
+            int second = (int)secondSymbol;
+
+            if (second < first)
+            {
+                flag = false;
+            }
+            if (!flag)
+            {
+                for (int i = second + 1; i < first; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+            else
+            {
+                for (int i = first + 1; i < second; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+            }
+        }
+    }
+}
