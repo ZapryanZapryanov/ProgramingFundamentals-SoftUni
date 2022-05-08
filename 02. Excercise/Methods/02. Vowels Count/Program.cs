a820b514e71322e6686c3cc1cddb274330111636
zaprian1223 <zaprian1223@gmail.com>
2022-05-08 13:32:25 +0300

    Added a solution

---
+++ 02. Excercise/Methods/02. Vowels Count/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(GetVowels(word));
+        }
+        static int GetVowels(string word)
+        {
+            char[] vowels = new char[]
+            {
+                'a',
+                'e',
+                'i',
+                'o',
+                'u'
+            };
+            int counter = 0;
+            foreach (char charucter in word.ToLower())
+            {
+                if (vowels.Contains(charucter))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
