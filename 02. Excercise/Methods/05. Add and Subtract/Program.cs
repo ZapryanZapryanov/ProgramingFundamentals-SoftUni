ff0a5a1fe2d5be3477d342608bbe8cfc6e47262c
zaprian1223 <zaprian1223@gmail.com>
2022-05-08 13:33:12 +0300

    Added a solution

---
+++ 02. Excercise/Methods/05. Add and Subtract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int firstSum = GetFirstSum(firstNum, secondNum);
+            int result = GetAllSum(firstSum, thirdNum);
+            Console.WriteLine(result);
+        }
+        static int GetFirstSum(int one, int two)
+        {
+            return one + two;
+        }
+        static int GetAllSum(int allSum, int thirdNum)
+        {
+            return allSum - thirdNum;
+        }
+    }
+}
