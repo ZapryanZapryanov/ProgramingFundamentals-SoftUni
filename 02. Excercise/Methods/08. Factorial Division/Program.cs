ad74dc10f9cbd15ce21a9fddd75df601085eeedc
zaprian1223 <zaprian1223@gmail.com>
2022-05-09 17:24:52 +0300

    Added a solution

---
+++ 02. Excercise/Methods/08. Factorial Division/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+            double firstFac = GetFirstSumFactorial(first);
+            double secondFac = GetFirstSumFactorial(second);
+            double result = firstFac / secondFac;
+            Console.WriteLine($"{result:f2}");
+        }
+        static double GetFirstSumFactorial(double first)
+        {
+
+            double result = 1;
+            for (int i = 1; i <= first; i++)
+            {
+                result *= i;
+
+            }
+
+            return result;
+        }
+        static double GetSecondSumFactorial(double second)
+        {
+
+            double result = 1;
+            for (int i = 1; i <= second; i++)
+            {
+                result *= i;
+
+            }
+
+            return result;
+        }
+    }
+}
