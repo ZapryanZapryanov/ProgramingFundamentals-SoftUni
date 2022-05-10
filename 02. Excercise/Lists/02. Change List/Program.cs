0fe7ebf3249e599c8e65249a91bce733c21de17c
zaprian1223 <zaprian1223@gmail.com>
2022-05-10 17:43:13 +0300

    Added a solution

---
+++ 02. Excercise/Lists/02. Change List/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _02._Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            string comand = Console.ReadLine();
+
+            while (comand != "end")
+            {
+                string[] input = comand.Split(" ");
+                if (input[0] == "Delete")
+                {
+                    int element = int.Parse(input[1]);
+                    list.RemoveAll(x => x == element);
+                }
+                else if (input[0] == "Insert")
+                {
+                    int element = int.Parse(input[1]);
+                    int position = int.Parse(input[2]);
+                    list.Insert(position, element);
+
+                }
+
+
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
