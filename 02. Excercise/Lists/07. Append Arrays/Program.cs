b0c69f66d7a70156365eb63d13953ad7503ca6ac
zaprian1223 <zaprian1223@gmail.com>
2022-05-11 17:34:34 +0300

    Added a solution

---
+++ 02. Excercise/Lists/07. Append Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().
+                Split('|', StringSplitOptions.RemoveEmptyEntries).
+                ToList();
+            var newList = new List<int>();
+            var list = new List<int>();
+            for (int i = input.Count - 1; i >= 0; i--)
+            {
+                newList = input[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                foreach (var item in newList)
+                {
+                    list.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
