6fc94928a2471b43e73a0fed0c402814cea900b1
zaprian1223 <zaprian1223@gmail.com>
2022-05-12 17:38:40 +0300

    Added a solution

---
+++ 02. Excercise/Objects and Classes/04. Students/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<SomeStudent> listStudent = new List<SomeStudent>();
+            for (int i = 1; i <= n; i++)
+            {
+                string[] infoStudents = Console.ReadLine().Split(" ").ToArray();
+                string firstName = infoStudents[0];
+                string lastName = infoStudents[1];
+                double grade = double.Parse(infoStudents[2]);
+                SomeStudent newStudents = new SomeStudent(firstName, lastName, grade);
+                listStudent.Add(newStudents);
+
+            }
+
+
+            List<SomeStudent> sortedStudents = listStudent.OrderByDescending(t => t.Grade).ToList();
+
+            foreach (var item in sortedStudents)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        class SomeStudent
+        {
+            public SomeStudent(string firstName, string lastName, double grade)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.Grade = grade;
+            }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public double Grade { get; set; }
+
+            public override string ToString()
+            {
+                return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+            }
+        }
+    }
+}
