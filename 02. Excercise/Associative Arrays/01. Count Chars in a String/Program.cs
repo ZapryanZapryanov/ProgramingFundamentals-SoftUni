1184aef0201875927fcb3720b054cd312d746c0d
zaprian1223 <zaprian1223@gmail.com>
2022-05-13 17:56:38 +0300

    Added a solution

---
+++ 02. Excercise/Associative Arrays/01. Count Chars in a String/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> first = Console.ReadLine()
+               .Split()
+                   .ToList();
+
+            if (first.Count > 1)
+
+            {
+
+                Merge(first);
+
+            }
+
+            char[] word = first[0].ToCharArray();
+
+            Dictionary<char, int> numberChar = new Dictionary<char, int>();
+
+            foreach (var kvp in word)
+
+            {
+
+                if (!numberChar.ContainsKey(kvp))
+
+                {
+
+                    numberChar[kvp] = 0;
+
+                }
+
+
+
+                numberChar[kvp]++;
+
+            }
+
+            foreach (var kvp in numberChar)
+
+            {
+
+                Console.WriteLine("{0} -> {1}", kvp.Key, kvp.Value);
+
+            }
+
+        }
+
+        public static void Merge(List<string> first)
+
+        {
+
+            StringBuilder currentWord = new StringBuilder();
+
+            for (int i = 0; i < first.Count; i++)
+
+            {
+
+                currentWord.Append(first[i]);
+
+            }
+
+            first.RemoveRange(0, first.Count - 1);
+
+            first.Insert(0, currentWord.ToString());
+        }
+    }
+}
