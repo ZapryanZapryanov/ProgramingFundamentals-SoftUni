f840c3a639ce0e7d393feed9902adb1c1cc90dde
zaprian1223 <zaprian1223@gmail.com>
2022-05-13 17:56:57 +0300

    Added a solution

---
+++ 02. Excercise/Associative Arrays/02. A Miner Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            string comand;
+            while ((comand = Console.ReadLine()) != "stop")
+            {
+                int quality = int.Parse(Console.ReadLine());
+                if (dictionary.ContainsKey(comand))
+                {
+                    dictionary[comand] += quality;
+                }
+                else
+                {
+                    dictionary[comand] = quality;
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
