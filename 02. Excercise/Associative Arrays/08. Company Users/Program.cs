a4151cc556615c92a21e953434340795a0155391
zaprian1223 <zaprian1223@gmail.com>
2022-05-14 11:05:41 +0300

    Added a solution

---
+++ 02. Excercise/Associative Arrays/08. Company Users/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+
+            Dictionary<string, List<string>> company = new Dictionary<string, List<string>>();
+
+            while (comand != "End")
+            {
+                string[] elements = comand.Split(" -> ");
+                string nameFirm = elements[0];
+                string idEmployee = elements[1];
+                List<string> newId = new List<string>() { idEmployee };
+
+                if (company.ContainsKey(nameFirm))
+                {
+                    if (company[nameFirm].Contains(idEmployee))
+                    {
+                        comand = Console.ReadLine();
+                        continue;
+                    }
+                    company[nameFirm].Add(idEmployee);
+                }
+                else
+                {
+                    company.Add(nameFirm, newId);
+
+                }
+
+
+
+                comand = Console.ReadLine();
+            }
+
+            foreach (var item in company)
+            {
+                Console.WriteLine($"{item.Key}");
+
+                foreach (var items in item.Value)
+                {
+                    Console.WriteLine($"-- {string.Join(" ", items)}");
+                }
+
+            }
+        }
+    }
+}
