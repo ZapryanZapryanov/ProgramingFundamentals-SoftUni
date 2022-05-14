20bb64d608a195f549bd9396e3d705f9dd3bfdcf
zaprian1223 <zaprian1223@gmail.com>
2022-05-14 11:05:07 +0300

    Added a solution

---
+++ 02. Excercise/Associative Arrays/06. Courses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+
+            Dictionary<string, List<string>> course = new Dictionary<string, List<string>>();
+
+            while (comand != "end")
+            {
+                string[] elements = comand.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+                string courseName = elements[0];
+                string studentsName = elements[1];
+
+                if (course.ContainsKey(courseName))
+                {
+                    course[courseName].Add(studentsName);
+                }
+                else
+                {
+                    List<string> newList = new List<string>() { studentsName };
+                    course.Add(courseName, newList);
+                }
+
+                comand = Console.ReadLine();
+            }
+
+            foreach (var item in course)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count}");
+                foreach (var nextItem in item.Value)
+                {
+                    Console.WriteLine($"-- {string.Join(" ", nextItem)}");
+                }
+            }
+        }
+    }
+}
