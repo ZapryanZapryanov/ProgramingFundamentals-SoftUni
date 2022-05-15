3b4c119509a0d3e26e9f4d9848446dbd494a1be7
zaprian1223 <zaprian1223@gmail.com>
2022-05-15 16:38:10 +0300

    Added a solution

---
+++ 02. Excercise/Text Processing/07. String Explosion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace _07._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine();
+
+            StringBuilder newText = new StringBuilder();
+
+            int exp = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+
+                if (words[i] == '>')
+                {
+                    exp += words[i + 1];
+                    exp -= 48;
+                    newText.Append('>');
+                }
+                else if (exp != 0)
+                {
+
+                    exp--;
+                }
+                else
+                {
+                    newText.Append(words[i]);
+                }
+            }
+            Console.WriteLine(newText);
+        }
+    }
+}
