a9d3a0a1dd30593665b1c3c2ae03946cc0107f4f
zaprian1223 <zaprian1223@gmail.com>
2022-05-15 16:37:10 +0300

    Added a solution

---
+++ 02. Excercise/Text Processing/03. Extract File/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fail = Console.ReadLine();
+
+            int index = fail.LastIndexOf("\\");
+            fail = fail.Substring(index + 1);
+            int indexLast = fail.LastIndexOf(".");
+
+            string firstElement = fail.Substring(0, indexLast);
+            string secondElement = fail.Substring(indexLast + 1);
+            Console.WriteLine($"File name: {firstElement}");
+            Console.WriteLine($"File extension: {secondElement}");
+        }
+    }
+}
