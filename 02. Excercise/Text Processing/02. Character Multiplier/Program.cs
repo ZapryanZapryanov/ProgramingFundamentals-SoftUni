1b23d1cda5d9cd030d5d8185a140af7bac3ec36d
zaprian1223 <zaprian1223@gmail.com>
2022-05-15 16:36:56 +0300

    Added a solution

---
+++ 02. Excercise/Text Processing/02. Character Multiplier/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ");
+            int sum = 0;
+            int counter = 0;
+            if (words[0].Length > words[1].Length)
+            {
+                for (int i = 0; i < words[1].Length; i++)
+                {
+                    sum += (int)words[0][i] * (int)words[1][i];
+                    counter++;
+
+                }
+                for (int i = counter; i < words[0].Length; i++)
+                {
+                    sum += (int)words[0][i];
+                }
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                for (int i = 0; i < words[0].Length; i++)
+                {
+                    sum += (int)words[0][i] * (int)words[1][i];
+                    counter++;
+
+                }
+                for (int i = counter; i < words[1].Length; i++)
+                {
+                    sum += (int)words[1][i];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
