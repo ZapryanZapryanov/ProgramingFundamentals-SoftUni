52a286147fb7a3e5c60bcda286d0ff23600fb7fc
zaprian1223 <zaprian1223@gmail.com>
2022-05-16 13:03:01 +0300

    Added a solution

---
+++ 02. Excercise/Regular Expressions/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            string validText = @"\%(?<name>[A-Z][a-z]+)%[^\|\$\%\.]*?<(?<product>\w+)>[^\|\$\%\.]*?\|(?<count>[0-9]+)\|[^\|\$\%\.]*?(?<price>\d+\.?\d+)\$";
+            decimal sum = 0m;
+            while (comand != "end of shift")
+            {
+                Match valid = Regex.Match(comand, validText);
+
+                if (valid.Success)
+                {
+                    string name = valid.Groups["name"].Value;
+                    string product = valid.Groups["product"].Value;
+                    int count = int.Parse(valid.Groups["count"].Value);
+                    decimal price = decimal.Parse(valid.Groups["price"].Value);
+
+                    Console.WriteLine($"{name}: {product} - {(decimal)count * price:f2}");
+                    sum += (decimal)count * price;
+
+                }
+
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {sum:f2}");
+        }
+    }
+}
