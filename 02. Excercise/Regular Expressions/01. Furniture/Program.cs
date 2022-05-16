9f98e269a1de5d77e3d3b34e9c36b0cfbe04d986
zaprian1223 <zaprian1223@gmail.com>
2022-05-16 13:02:20 +0300

    Added a solution

---
+++ 02. Excercise/Regular Expressions/01. Furniture/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string regexName = @">>(?<name>[A-Za-z]+)<<(?<price>\d+\.?\d+)!(?<quantity>\d+)";
+            decimal allPrice = 0m;
+            Console.WriteLine("Bought furniture:");
+            while (input != "Purchase")
+            {
+                Match matches = Regex.Match(input, regexName);
+                if (matches.Success)
+                {
+                    string name = matches.Groups["name"].Value;
+                    decimal price = decimal.Parse(matches.Groups["price"].Value);
+                    int quantity = int.Parse(matches.Groups["quantity"].Value);
+                    allPrice += price * quantity;
+
+                    Console.WriteLine(name);
+
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total money spend: {allPrice:f2}");
+        }
+    }
+}
