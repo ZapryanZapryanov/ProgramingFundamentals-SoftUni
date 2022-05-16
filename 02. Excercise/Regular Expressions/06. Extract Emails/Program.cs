0accc47a5b3909f0274ea0d382dbaed29d71208a
zaprian1223 <zaprian1223@gmail.com>
2022-05-16 13:03:51 +0300

    Added a solution

---
+++ 02. Excercise/Regular Expressions/06. Extract Emails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string patern = @"(?<=\s)(?<user>[A-Za-z0-9]+[.-]*\w*)*@([a-z]+([.-][a-z]*)*(\.[a-z]{2,}))";
+            MatchCollection words = Regex.Matches(text, patern);
+
+            foreach (Match item in words)
+            {
+                Console.WriteLine($"{item.Value}");
+            }
+        }
+    }
+}
