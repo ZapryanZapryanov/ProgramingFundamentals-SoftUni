ec682126e5b8ac14b9f5508469e83ccde98944b3
zaprian1223 <zaprian1223@gmail.com>
2022-05-17 15:47:48 +0300

    Added a solution

---
+++ 03. More Exercises/Basic Syntax, Conditional Statements and Loops/01. Sort Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[3];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+
+            array1 = array1.OrderByDescending(x => x).ToArray();
+
+            foreach (var item in array1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
