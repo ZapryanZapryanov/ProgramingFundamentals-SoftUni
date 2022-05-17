4173dcb673cbc89eb54b31ed47658ecf72bf8481
zaprian1223 <zaprian1223@gmail.com>
2022-05-17 15:48:03 +0300

    Added a solution

---
+++ 03. More Exercises/Basic Syntax, Conditional Statements and Loops/02. Last Digit/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _02._Last_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            var getLast = numbers % 10;
+            //string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            if (getLast == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if (getLast == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if (getLast == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else if (getLast == 4)
+            {
+                Console.WriteLine("four");
+            }
+            else if (getLast == 5)
+            {
+                Console.WriteLine("five");
+            }
+            else if (getLast == 6)
+            {
+                Console.WriteLine("six");
+            }
+            else if (getLast == 7)
+            {
+                Console.WriteLine("seven");
+            }
+            else if (getLast == 8)
+            {
+                Console.WriteLine("eight");
+            }
+            else if (getLast == 9)
+            {
+                Console.WriteLine("nine");
+            }
+            else if (getLast == 0)
+            {
+                Console.WriteLine("zero");
+            }
+
+        }
+    }
+}
