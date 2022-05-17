9114aab7a88cb1dc3e817dd75c3e6f42b1609435
zaprian1223 <zaprian1223@gmail.com>
2022-05-17 15:48:37 +0300

    Added a solution

---
+++ 03. More Exercises/Basic Syntax, Conditional Statements and Loops/04. Reverse String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            //double[] numbers = new double[array1.Length];  Преобразуване към числа
+
+            var text = word.ToArray().Reverse();
+            Console.WriteLine(string.Join("", text));
+        }
+    }
+}
