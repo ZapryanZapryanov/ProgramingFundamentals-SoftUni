db6eef4c61b19c80d849de3d6c7f298ae47d2e70
zaprian1223 <zaprian1223@gmail.com>
2022-05-17 15:48:52 +0300

    Added a solution

---
+++ 03. More Exercises/Basic Syntax, Conditional Statements and Loops/05. Messages/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace _05._Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string word = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                string nums = Console.ReadLine();
+
+                int len = nums.Length;
+                int first = int.Parse(nums[0].ToString()); //Намира първия елемент
+                int offset = (first - 2) * 3;
+
+                if (first == 8 || first == 9)
+                {
+                    offset++;
+                }
+
+                if (first == 0)
+                {
+                    word += (char)(first + 32);
+                    continue;
+                }
+
+                var idx = offset + len - 1;
+                word += (char)(idx + 97);
+
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
