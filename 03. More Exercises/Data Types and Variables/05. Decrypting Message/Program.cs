bd87cb8376168f56949e92606c722b603604e55d
zaprian1223 <zaprian1223@gmail.com>
2022-05-18 12:38:21 +0300

    Added a solution

---
+++ 03. More Exercises/Data Types and Variables/05. Decrypting Message/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int key = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+            string word = " ";
+
+            for (int i = 0; i < count; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                // Четем буквата след това добавяме (key) към Аски стойността на буквата
+                // и намираме позицията на новополучената буква
+                char current = (char)(letter + (char)key);
+                word += current;
+                //char current = char.Parse(letter);
+                //word = letter + (char)key;
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
