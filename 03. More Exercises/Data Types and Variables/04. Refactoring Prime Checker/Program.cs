045b7bce7778bd189cc30847c304afa4e0825d2d
zaprian1223 <zaprian1223@gmail.com>
2022-05-18 12:38:03 +0300

    Added a solution

---
+++ 03. More Exercises/Data Types and Variables/04. Refactoring Prime Checker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04._Refactoring_Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= num; i++)
+            {
+                bool IsTrue = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        IsTrue = false;
+                        break;
+                    }
+                }
+                if (IsTrue)
+                {
+                    Console.WriteLine($"{i} -> true");
+                }
+                else if (!IsTrue)
+                {
+                    Console.WriteLine($"{i} -> false");
+                }
+
+            }
+
+        }
+    }
+}
