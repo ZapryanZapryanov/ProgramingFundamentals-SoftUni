8e7667bd170bdba5325d1b0ff8431ef6f9c1350e
zaprian1223 <zaprian1223@gmail.com>
2022-05-18 12:37:14 +0300

    Added a solution

---
+++ 03. More Exercises/Data Types and Variables/01. Data Type Finder/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int a;
+            float b;
+            char c;
+            bool d;
+
+            while (command != "END")
+            {
+                //num = double.Parse(command);              
+                bool checkInt = int.TryParse(command, out a);//Пробва да конвертира стринга към дадения тип данни
+                bool checkFloat = float.TryParse(command, out b);
+                bool checkChar = char.TryParse(command, out c);
+                bool checkBool = bool.TryParse(command, out d);
+                //bool checkStr = IsString(command);
+
+                if (checkInt)
+                {
+                    Console.WriteLine($"{command} is integer type");
+                }
+                else if (checkFloat)
+                {
+                    Console.WriteLine($"{command} is floating point type");
+                }
+                else if (checkChar)
+                {
+                    Console.WriteLine($"{command} is character type");
+                }
+                else if (checkBool)
+                {
+                    Console.WriteLine($"{command} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{command} is string type");
+                }
+                command = Console.ReadLine();
+
+            }
+        }
+    }
+}
