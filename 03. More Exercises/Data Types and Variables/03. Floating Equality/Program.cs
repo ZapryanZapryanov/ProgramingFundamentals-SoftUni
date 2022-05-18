1654a273e7b116b30ea96bb68b9df7c83d405feb
zaprian1223 <zaprian1223@gmail.com>
2022-05-18 12:37:45 +0300

    Added a solution

---
+++ 03. More Exercises/Data Types and Variables/03. Floating Equality/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double result = 0;
+
+            if (a >= b)
+            {
+                result = a - b;
+                if (result > 0.000001)
+                {
+                    Console.WriteLine("False");
+                }
+                else
+                {
+                    Console.WriteLine("True");
+                }
+            }
+            else if (a <= b)
+            {
+                result = b - a;
+                if (result > 0.000001)
+                {
+                    Console.WriteLine("False");
+                }
+                else
+                {
+                    Console.WriteLine("True");
+                }
+            }
+        }
+    }
+}
