c2c57c139b8441f62778934e693f22a7792bfb37
zaprian1223 <zaprian1223@gmail.com>
2022-05-19 15:19:30 +0300

    Added a solution

---
+++ 03. More Exercises/Arrays/04. Fold and Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int fromLeft = array1.Length / 4;
+
+            int[] result = new int[fromLeft * 2];
+
+            for (int i = 0; i < fromLeft; i++)
+            {
+                result[i] = array1[i + fromLeft] + array1[fromLeft - 1 - i];
+
+                result[i + fromLeft] = array1[i + 2 * fromLeft] + array1[array1.Length - 1 - i];
+            }
+
+            Console.WriteLine(String.Join(' ', result));
+        }
+    }
+}
