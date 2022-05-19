517b6cd8affe4c0a4afea21e110c7ffb447889d6
zaprian1223 <zaprian1223@gmail.com>
2022-05-19 15:18:29 +0300

    Added a solution

---
+++ 03. More Exercises/Arrays/02. Pascal Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            if (rows == 1)
+            {
+                Console.WriteLine($"1");
+            }
+            else
+            {
+                Console.WriteLine($"1");  
+                Console.WriteLine($"1 1");
+
+                int[] current = { 1, 1 };
+
+                for (int row = 3; row <= rows; row++)
+                {
+
+                    int[] num = new int[row];
+                    num[0] = 1;
+
+                    for (int col = 1; col < num.Length - 1; col++)
+                    {
+                        num[col] = current[col - 1] + current[col];
+                        num[col + 1] = 1;
+
+                    }
+                    current = num;
+                    Console.WriteLine(string.Join(" ", current));
+                }
+            }
+        }
+    }
+}
