895fc39d51fcf2bc837d247e9e79f7b4581b8a86
zaprian1223 <zaprian1223@gmail.com>
2022-05-19 15:19:47 +0300

    Added a solution

---
+++ 03. More Exercises/Arrays/05. Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Longest_Increasing_Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] list;
+            int[] current = new int[numbers.Length];
+            int[] prev = new int[numbers.Length];
+            int maxLength = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                current[i] = 1;
+                prev[i] = -1;
+
+               
+                for (int j = 0; j < i; j++)
+                {
+                    
+                    if (numbers[j] < numbers[i] && current[j] >= current[i])
+                    {
+                        current[i] = 1 + current[j];
+                        prev[i] = j; 
+                    }
+                }
+                
+                if (current[i] > maxLength)
+                {
+                    maxLength = current[i];
+                    lastIndex = i;
+                }
+            }
+            list = new int[maxLength];
+            for (int i = 0; i < maxLength; i++)
+            {
+                list[i] = numbers[lastIndex];
+                lastIndex = prev[lastIndex];
+            }
+            Array.Reverse(list);
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
