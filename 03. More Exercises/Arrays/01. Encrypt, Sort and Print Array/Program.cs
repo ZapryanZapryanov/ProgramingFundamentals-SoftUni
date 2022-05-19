1ec8a6d955821b1689e59a4c5219b1417bbcdcab
zaprian1223 <zaprian1223@gmail.com>
2022-05-19 15:17:57 +0300

    Added a solution

---
+++ 03. More Exercises/Arrays/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] sequence = new int[count];
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                string word = Console.ReadLine();
+                int sumVow = 0;
+                int sumCon = 0;
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (vowels.Contains(word[j]))
+                    {
+                        sumVow += word[j] * word.Length;
+                    }
+                    else
+                    {
+                        sumCon += word[j] / word.Length;
+                    }
+                }
+                int total = sumCon + sumVow;
+                sequence[i] = total;
+
+            }
+
+            Array.Sort(sequence);
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                Console.WriteLine(sequence[i]);
+            }
+        }
+    }
+}
