f599c00488b7fd369dc7d8de9ea06c382048a17b
zaprian1223 <zaprian1223@gmail.com>
2022-05-19 15:19:16 +0300

    Added a solution

---
+++ 03. More Exercises/Arrays/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03._Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(input));
+        }
+
+        static int Fibonacci(int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+
+            }
+            if (num == 1)
+            {
+                return 1;
+            }
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+    }
+}
