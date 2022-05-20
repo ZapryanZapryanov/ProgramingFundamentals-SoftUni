88366c46cee5f507418372397b9bd6d24b032676
zaprian1223 <zaprian1223@gmail.com>
2022-05-20 17:40:06 +0300

    Added a solution

---
+++ 03. More Exercises/Methods/04. Tribonacci Sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04._Tribonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write($"{GetTribonacci(i)} ");
+            }
+
+        }
+
+        static int GetTribonacci(int num)
+        {
+            if (num <= 2)
+            {
+                return 1;
+            }
+            if (num == 3)
+            {
+                return 2;
+            }
+            else
+            {
+                return GetTribonacci(num - 1) + GetTribonacci(num - 2) + GetTribonacci(num - 3);
+            }
+        }
+    }
+}
