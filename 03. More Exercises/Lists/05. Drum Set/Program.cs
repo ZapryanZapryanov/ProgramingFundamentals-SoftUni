efb4ddcf7d3929016cc77b2921864381bd7de76d
zaprian1223 <zaprian1223@gmail.com>
2022-05-21 14:04:08 +0300

    Added a solution

---
+++ 03. More Exercises/Lists/05. Drum Set/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Drum_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> fullList = new List<int>(numbers);
+            //List<int> test2 = new List<int>(numbers);
+            //test.AddRange(numbers);
+
+            string command = Console.ReadLine();
+
+            while (command != "Hit it again, Gabsy!")
+            {
+                int power = int.Parse(command);
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    // 55 111 3 5 8 50                  
+                    numbers[i] -= power;
+                    if (numbers[i] <= 0)
+                    {
+                        var idx = numbers.FindIndex(x => x <= 0);
+                        var initialDrumValue = fullList.ElementAt(idx);
+                        var elemZero = numbers.ElementAt(idx);
+                        var purchase = initialDrumValue * 3;
+
+                        if (savings - purchase >= 0)
+                        {
+                            numbers[i] = initialDrumValue;
+                            savings -= purchase;
+                        }
+                        else if (elemZero <= 0)
+                        {
+                            numbers.RemoveAt(idx);
+                            fullList.RemoveAt(idx);
+                            i--; //Намалявам индекса, защото става разбъркване от премахването
+                        }
+                    }
+
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
