53ed7db7aa371253877427fec868678649284974
zaprian1223 <zaprian1223@gmail.com>
2022-05-26 13:46:54 +0300

    Added a solution

---
+++ 03. More Exercises/Text Processing/05. HTML/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titlePage = Console.ReadLine();
+            string contentPage = Console.ReadLine();
+            string input = Console.ReadLine();
+            List<string> comment = new List<string>();
+
+            while (input != "end of comments")
+            {
+                comment.Add(input);
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {titlePage}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"    {contentPage}");
+            Console.WriteLine("</article>");
+            for (int i = 0; i < comment.Count; i++)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"    {comment[i]}");
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}
