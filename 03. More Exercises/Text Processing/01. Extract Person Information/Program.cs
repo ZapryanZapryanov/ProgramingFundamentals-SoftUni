a1062415072a9f69571754bcd0bc5f59ac7b92ff
zaprian1223 <zaprian1223@gmail.com>
2022-05-26 13:45:55 +0300

    Added a solution

---
+++ 03. More Exercises/Text Processing/01. Extract Person Information/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nLines; i++)
+            {
+                string comand = Console.ReadLine();
+                int firstIndexName = comand.IndexOf("@");
+                int lastIndexName = comand.LastIndexOf("|");
+                string name = comand.Substring(firstIndexName + 1, lastIndexName - 1 - firstIndexName);
+                int firstIndexAge = comand.IndexOf("#");
+                int lastIndexAge = comand.LastIndexOf("*");
+                string age = comand.Substring(firstIndexAge + 1, lastIndexAge - 1 - firstIndexAge);
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
